128fdfa46d0ebba66dc9c90af4e94400d8ba3ed1
rafael <rafael@gmail.com>
2026-04-27 12:29:24 +0100

    Add academy_management module with basic structure

---
+++ addons/academy_management/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Academy Management',
+    'version': '1.0',
+    'category': 'Education',
+    'summary': 'Gestión de academia',
+    'description': '''
+        Módulo para gestionar estudiantes, cursos y matrículas
+    ''',
+    'author': 'Tu Nombre',
+    'website': '',
+    'depends': ['base'],
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
